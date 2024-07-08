22c185322ce53216d4c85a1e7ceb7e5cb64798d7
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-07-08 19:55:23 -0400

    Added functionality for invisible captcha

--- server.js
+++ server.js
@@ -3,20 +3,29 @@ const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
 const app = express()
-const SITE_SECRET = process.env.SITE_SECRET
+const CHECKBOX_SITE_SECRET = process.env.CHECKBOX_SITE_SECRET
+const INVISIBLE_SITE_SECRET = process.env.INVISIBLE_SITE_SECRET
 
 app.use(cors())
 app.use(express.json())
 
-app.post('/verify', async (request, response) => {
+app.post('/verify/checkbox', async (request, response) => {
   const { captchaValue } = request.body
   const { data } = await axios.post(
-    `https://www.google.com/recaptcha/api/siteverify?secret=${SITE_SECRET}&response=${captchaValue}`,
+    `https://www.google.com/recaptcha/api/siteverify?secret=${CHECKBOX_SITE_SECRET}&response=${captchaValue}`,
   )
   response.send(data)
 })
 
-const port = (process.env.PORT || 5000);
+app.post('/verify/invisible', async (request, response) => {
+  const { captchaValue } = request.body
+  const { data } = await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${INVISIBLE_SITE_SECRET}&response=${captchaValue}`,
+  )
+  response.send(data)
+})
+
+const port = (process.env.PORT || 5001);
 app.listen(port, () => {
   console.log(`reCaptcha Server listening at ${port}`)
 })
