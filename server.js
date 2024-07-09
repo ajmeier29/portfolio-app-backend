e8695c414cb38f001238d70b7b26ad9a2a78d512
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-07-08 21:51:45 -0400

    added octo email endpoint

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const axios = require('axios')
 const app = express()
 const CHECKBOX_SITE_SECRET = process.env.CHECKBOX_SITE_SECRET
 const INVISIBLE_SITE_SECRET = process.env.INVISIBLE_SITE_SECRET
+const JUSTIN_EMAIL_OCTO_API_KEY = process.env.JUSTIN_EMAIL_OCTO_API_KEY
+const JUSTIN_EMAIL_OCTO_LIST_ID = process.env.JUSTIN_EMAIL_OCTO_LIST_ID
 
 app.use(cors())
 app.use(express.json())
@@ -23,6 +25,34 @@ app.post('/verify/invisible', async (request, response) => {
     `https://www.google.com/recaptcha/api/siteverify?secret=${INVISIBLE_SITE_SECRET}&response=${captchaValue}`,
   )
   response.send(data)
+})
+
+app.post('/subscribe/justin', async (request, response) => {
+  const emailAddress = request.body.email_address
+  console.log(`reqBody: ${JSON.stringify(emailAddress)}`)
+  console.log(`JUSTIN_EMAIL_OCTO_API_KEY: ${JUSTIN_EMAIL_OCTO_API_KEY}`)
+  const url = `https://emailoctopus.com/api/1.5/lists/${JUSTIN_EMAIL_OCTO_LIST_ID}/contacts`;
+  const subscriberData =
+  {
+    api_key: JUSTIN_EMAIL_OCTO_API_KEY,
+    email_address: emailAddress
+  }
+  console.log(`subscriberData: ${JSON.stringify(subscriberData)}`)
+  var respData = ''
+  await axios.post(url, subscriberData, {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      respData = response.data
+      console.log('New subscriber added:', response.data);
+    })
+    .catch(error => {
+      console.error('Error adding subscriber:', error);
+    });
+  response.send(respData)
 })
 
 const port = (process.env.PORT || 5001);
